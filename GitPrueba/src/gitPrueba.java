a8cdd5cfcb5b55deab1337d983c10aca3b61e618
IEUser <IEUser@192.168.24.128>
2018-05-08 09:55:10 -0700

    nueva rama

--- GitPrueba/src/gitPrueba.java
+++ GitPrueba/src/gitPrueba.java
@@ -3,5 +3,6 @@ public class gitPrueba {
 	public static void main(String[] args) {
 			System.out.println("Hola Git Prueba");
 			System.out.println("Adios Git Prueba");
+			System.out.println("Hola nueva rama");
 }
 }
